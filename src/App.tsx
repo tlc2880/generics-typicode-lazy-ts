06b42c9228f73750669de6f4b34858ab21b02e05
tlc2880 <tommylcao@gmail.com>
2023-09-15 09:24:20 -0700

    Add List component to display todos table after selection of typicode user

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import List from "./List"
+
+import { usersType, todosType } from './app.Types'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([]);
+  const [todos, setTodos] = useState([]);
 
+  const [selectedUser, setSelectedUser] = useState("");
+  const [selectedTodos, setSelectedTodos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((result) => setUsers(result));
+
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((result) => setTodos(result));
+  }, []);
+
+  const selectUser = (id: number) => {
+     const filterTodos = todos.filter((u: todosType) => u.userId === id);
+
+    setSelectedUser(id.toString());
+    setSelectedTodos(filterTodos);
+
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <table>
+        <thead>
+          <tr>
+            <th>User Id</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users?.map((user: usersType) => (
+            <tr key={user?.id} onClick={() => selectUser(user?.id)}>
+              <td>{user?.id}</td>
+              <td>{user?.name}</td>
+              <td>{user?.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <List items={selectedTodos} />
+
     </>
   )
 }
