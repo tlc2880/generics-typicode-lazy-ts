87c2536489bd9a39fcc128105f949148980f4b07
tlc2880 <tommylcao@gmail.com>
2023-09-17 07:09:54 -0700

    Add fetch albums table and ListItems

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,47 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import List from "./List"
 
-import { usersType, todosType, postsType } from './app.Types'
+import { usersType, todosType, postsType, albumsType } from './app.Types'
 
 function App() {
   const [users, setUsers] = useState([]);
   const [todos, setTodos] = useState([]);
   const [posts, setPosts] = useState([]);
+  const [albums, setAlbums] = useState([]);
 
   // @ts-ignore
   const [selectedUser, setSelectedUser] = useState("");
   const [selectedTodos, setSelectedTodos] = useState([]);
   const [selectedPosts, setSelectedPosts] = useState([]);
+  const [selectedAlbums, setSelectedAlbums] = useState([]);
+  
+  const ListItems = [
+    {
+      userId: 3,
+      title: "☕ Coffee",
+      total: 12,
+    },
+    {
+      userId: 3,
+      title: "🌮 Tacos",
+      total: 10,
+    },
+    {
+      userId: 3,
+      title: "💻 Code",
+      total: 9,
+    },
+    {
+      userId: 3,
+      title: "⛵ Sailboat",
+      total: 6,
+    },
+    {
+      userId: 3,
+      title: "✈ Plane",
+      total: 3,
+    }
+  ];
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -26,16 +56,21 @@ function App() {
       fetch("https://jsonplaceholder.typicode.com/posts")
       .then((res) => res.json())
       .then((result) => setPosts(result));
+
+      fetch("https://jsonplaceholder.typicode.com/albums")
+      .then((res) => res.json())
+      .then((result) => setAlbums(result));
   }, []);
 
   const selectUser = (id: number) => {
      const filterTodos = todos.filter((u: todosType) => u.userId === id);
      const filterPosts = posts.filter((u: postsType) => u.userId === id);
+     const filterAlbums = albums.filter((u: albumsType) => u.userId === id);
 
     setSelectedUser(id.toString());
     setSelectedTodos(filterTodos);
     setSelectedPosts(filterPosts);
-
+    setSelectedAlbums(filterAlbums);
   };
   return (
     <>
@@ -60,6 +95,8 @@ function App() {
 
       <List items={selectedTodos} />
       <List items={selectedPosts} />
+      <List items={selectedAlbums} />
+      <List items={ListItems} />
     </>
   )
 }
