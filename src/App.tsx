acad313eea70b782180c4958623454f434195bc2
tlc2880 <tommylcao@gmail.com>
2023-10-16 04:33:47 -0700

    Add lazy loading with error-boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,103 +1,31 @@
-import { useState, useEffect } from 'react'
+import { Routes, Route, useNavigate } from "react-router-dom"
+import { ErrorBoundary } from "react-error-boundary"
+import { Suspense } from "react"
+import Layout from "./lazy/Layout"
+import ErrorFallback from "./lazy/ErrorFallback"
+import SkeletonAdmin from './lazy/skeletons/SkeletonAdmin'
+import Admin from "./lazy/Admin"
+import Home from "./lazy/Home"
 import './App.css'
-import List from "./List"
-
-import { usersType, todosType, postsType, albumsType } from './app.Types'
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const [todos, setTodos] = useState([]);
-  const [posts, setPosts] = useState([]);
-  const [albums, setAlbums] = useState([]);
-
-  // @ts-ignore
-  const [selectedUser, setSelectedUser] = useState("");
-  const [selectedTodos, setSelectedTodos] = useState([]);
-  const [selectedPosts, setSelectedPosts] = useState([]);
-  const [selectedAlbums, setSelectedAlbums] = useState([]);
-  
-  const ListItems = [
-    {
-      userId: 3,
-      title: "☕ Coffee",
-      total: 12,
-    },
-    {
-      userId: 3,
-      title: "🌮 Tacos",
-      total: 10,
-    },
-    {
-      userId: 3,
-      title: "💻 Code",
-      total: 9,
-    },
-    {
-      userId: 3,
-      title: "⛵ Sailboat",
-      total: 6,
-    },
-    {
-      userId: 3,
-      title: "✈ Plane",
-      total: 3,
-    }
-  ];
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((result) => setUsers(result));
-
-    fetch("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => res.json())
-      .then((result) => setTodos(result));
-
-      fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((result) => setPosts(result));
-
-      fetch("https://jsonplaceholder.typicode.com/albums")
-      .then((res) => res.json())
-      .then((result) => setAlbums(result));
-  }, []);
-
-  const selectUser = (id: number) => {
-     const filterTodos = todos.filter((u: todosType) => u.userId === id);
-     const filterPosts = posts.filter((u: postsType) => u.userId === id);
-     const filterAlbums = albums.filter((u: albumsType) => u.userId === id);
-
-    setSelectedUser(id.toString());
-    setSelectedTodos(filterTodos);
-    setSelectedPosts(filterPosts);
-    setSelectedAlbums(filterAlbums);
-  };
+  const navigate = useNavigate()
   return (
-    <>
-      <table>
-        <thead>
-          <tr>
-            <th>User Id</th>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users?.map((user: usersType) => (
-            <tr key={user?.id} onClick={() => selectUser(user?.id)}>
-              <td>{user?.id}</td>
-              <td>{user?.name}</td>
-              <td>{user?.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-
-      <List items={selectedTodos} />
-      <List items={selectedPosts} />
-      <List items={selectedAlbums} />
-      <List items={ListItems} />
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="admin" element={
+          <ErrorBoundary
+            FallbackComponent={ErrorFallback}
+            onReset={() => navigate('/')}
+          >
+            <Suspense fallback={<SkeletonAdmin />}>
+              <Admin />
+            </Suspense>
+          </ErrorBoundary>}
+        />
+      </Route>
+    </Routes>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import { StrictMode } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+   </StrictMode>
 )
